63473052d993603a0af90a7ed8e0a9d19af26683
Robby Reid <rareid@c28-0117-18.ad.mtu.edu>
2016-09-02 14:37:02 -0400

    added information about Robert Reid

--- code/participants.c
+++ code/participants.c
@@ -18,7 +18,14 @@ int
 main (int argc, char *argv[])
 {
   int i = 0;
-  printf ("%02d: Kim W Tracy\n", ++i);
+
+  printf ("%02d: Kim Tracy\n", ++i);
+  printf ("%02d: Justin Williams\n", ++i);
+  printf ("%02d: Steven Bertolucci\n", ++i);
+  printf ("%02d: Andrew Wolowiec\n", ++i);
+  printf ("%02d: Ben Closner\n", ++i); 
+  printf ("%02d: Baron Lenardson\n", ++i);
+  printf ("%02d: Jordan Gagnon\n", ++i);
   
   printf ("There are %i participants.\n", i);
   return 0;
--- participants.md
+++ participants.md
@@ -1,6 +1,10 @@
 //
-// Please put names in alphabetical order into this file
+// Please put names in alphabetical order (according to first name) into this file
 //
-
-
+Baron Lenardson
+Ben Closner
+Jordan Gagnon
+Joshua Prewett
+Justin Williams
 Kim Tracy
+Steven Bertolucci
