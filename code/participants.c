2cb1af9670b210f0f06e859dd781e8f458a2afc4
Gage Heeringa <grheerin@mtu.edu>
2016-09-04 13:29:24 -0400

    added Gage to participants

--- code/participants.c
+++ code/participants.c
@@ -35,7 +35,8 @@ main (int argc, char *argv[])
   printf ("%02d: Kyle Bray\n" , ++i);
   printf ("%02d: Jacob Striebel\n" , ++i);
   printf ("%02d: Andrew Fox\n" , ++i);
-  
+  printf ("%02d: Gage Heeringa\n" , ++i);
+
   printf ("There are %i participants.\n", i);
   return 0;
 } // main
