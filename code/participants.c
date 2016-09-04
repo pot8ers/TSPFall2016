85e6ca075144270416d6551eceaf7751bdc949c7
BushWookiee <jwwolosc@mtu.edu>
2016-09-04 18:50:44 -0400

    Added participant info for Jacob Woloschek

--- code/participants.c
+++ code/participants.c
@@ -38,6 +38,7 @@ main (int argc, char *argv[])
   printf ("%02d: Gage Heeringa\n" , ++i);
   printf ("%02d: Joseph Aldape\n" , ++i);
   printf ("%02d: Marcus Stojcevich\n" , ++i);
+  printf ("%02d: Jacob Woloschek\n" , ++i);
 
   printf ("There are %i participants.\n", i);
   return 0;
