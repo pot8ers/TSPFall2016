c6ff3c0942b093976251e3ea427c6f61686af8a2
Spencer Miskoviak <smiskoviak@gmail.com>
2016-09-05 12:59:02 -0400

    add Spencer Miskoviak to participants.c

--- code/participants.c
+++ code/participants.c
@@ -14,7 +14,7 @@
 // | Main |
 // +------+
 
-int 
+int
 main (int argc, char *argv[])
 {
   int i = 0;
@@ -23,12 +23,12 @@ main (int argc, char *argv[])
   printf ("%02d: Justin Williams\n", ++i);
   printf ("%02d: Steven Bertolucci\n", ++i);
   printf ("%02d: Andrew Wolowiec\n", ++i);
-  printf ("%02d: Ben Closner\n", ++i); 
+  printf ("%02d: Ben Closner\n", ++i);
   printf ("%02d: Dan Knenlein\n", ++i);
   printf ("%02d: Baron Lenardson\n", ++i);
   printf ("%02d: Jordan Gagnon\n", ++i);
   printf ("%02d: Joshua Prewett\n", ++i);
-  printf ("%02d: Eric Widmann\n" , ++i);  
+  printf ("%02d: Eric Widmann\n" , ++i);
   printf ("%02d: Michael Lay\n" , ++i);
   printf ("%02d: Michael LeCaptain\n" , ++i);
   printf ("%02d: Eric Widmann\n" , ++i);
@@ -39,6 +39,7 @@ main (int argc, char *argv[])
   printf ("%02d: Joseph Aldape\n" , ++i);
   printf ("%02d: Marcus Stojcevich\n" , ++i);
   printf ("%02d: Jacob Woloschek\n" , ++i);
+  printf ("%02d: Spencer Miskoviak\n" , ++i);
 
   printf ("There are %i participants.\n", i);
   return 0;
